c6b9bf65fad3154af56d94615e5d71912304bc7a
Tomasz Koba <tomaszkoba@outlook.com>
2022-03-20 21:12:29 +0100

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Tomasz Koba on 20/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
